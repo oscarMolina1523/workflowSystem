520aa5119fdec547c065b1e721f8d8b4c329f73e
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-09-25 17:17:01 -0600

    feat: add interface and class of connection to database

--- src/WebApi/container.ts
+++ src/WebApi/container.ts
@@ -4,8 +4,10 @@ import { container } from 'tsyringe';
 import { ISqlCommandOperationBuilder } from '../Infrastructure.Endpoint/interfaces/sqlCommandOperation.interface';
 import { IEntitiesService } from '../Infrastructure.Endpoint/interfaces/entitiesService.interface';
 import { EntitiesService } from '../Infrastructure.Endpoint/services/entitiesService';
+import { ISingletonSqlConnection } from '../Infrastructure.Endpoint/interfaces/database/dbConnection.interface';
+import { SingletonSqlConnection } from '../Infrastructure.Endpoint/database/dbConnection';
 
 // Registrar clases concretas
-
+container.registerSingleton<ISingletonSqlConnection>('ISingletonSqlConnection', SingletonSqlConnection);
 container.registerSingleton<IEntitiesService>('IEntityService', EntitiesService);
 container.register<ISqlCommandOperationBuilder>('IOperationBuilder', { useClass: SqlCommandOperationBuilder });
