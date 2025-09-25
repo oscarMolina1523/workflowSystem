b3a596718b21fb786da06fbae0044ab9a709740a
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-09-24 20:33:39 -0600

    refactor: use strong ids

--- src/Infrastructure.Endpoint/data/info/task.data.ts
+++ src/Infrastructure.Endpoint/data/info/task.data.ts
@@ -3,7 +3,7 @@ import Task from "../../../Domain.Endpoint/entities/task.model";
 
 export const seedTasks: Task[] = [
   {
-    id: "1",
+    id: "b90a4c28-568b-4dvs13-a4f6-82087a13c9e6",
     title: "Configurar proyecto",
     description: "Inicializar configuración base de Express + TS",
     status: Status.PENDING,
@@ -12,7 +12,7 @@ export const seedTasks: Task[] = [
     assignedTo: "developer",
   },
   {
-    id: "2",
+    id: "b90a4c28-568b-4b13-a4f6-82087a13c9ava",
     title: "Diseñar modelo de datos",
     description: "Definir tablas y relaciones para SQLite",
     status: Status.IN_PROGRESS,
