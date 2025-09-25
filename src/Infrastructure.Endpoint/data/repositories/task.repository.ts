da89e21b31f474a05cd4407047f0497c95d32841
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-09-25 17:19:19 -0600

    feat: add decorators of tsyringe and inject interfaces of sql command builder and sql connection

--- src/Infrastructure.Endpoint/data/repositories/task.repository.ts
+++ src/Infrastructure.Endpoint/data/repositories/task.repository.ts
@@ -1,21 +1,35 @@
+import { inject, injectable } from "tsyringe";
 import Task from "../../../Domain.Endpoint/entities/task.model";
 import { ITaskRepository } from "../../../Domain.Endpoint/interfaces/repositories/task.interface";
+import { ISingletonSqlConnection } from "../../interfaces/database/dbConnection.interface";
+import { ISqlCommandOperationBuilder } from "../../interfaces/sqlCommandOperation.interface";
 
+@injectable()
 export class TaskRepository implements ITaskRepository {
-    getAll(): Promise<Task[]> {
-        throw new Error("Method not implemented.");
-    }
-    getById(id: string): Promise<Task | null> {
-        throw new Error("Method not implemented.");
-    }
-    create(task: Task): Promise<void> {
-        throw new Error("Method not implemented.");
-    }
-    update(task: Task): Promise<void> {
-        throw new Error("Method not implemented.");
-    }
-    delete(task: Task): Promise<void> {
-        throw new Error("Method not implemented.");
-    }
+  private readonly _operationBuilder: ISqlCommandOperationBuilder;
+  private readonly _connection: ISingletonSqlConnection;
 
+  constructor(
+    @inject("IOperationBuilder") operationBuilder: ISqlCommandOperationBuilder,
+    @inject("ISingletonSqlConnection") connection: ISingletonSqlConnection
+  ) {
+    this._operationBuilder = operationBuilder;
+    this._connection = connection;
+  }
+
+  getAll(): Promise<Task[]> {
+    throw new Error("Method not implemented.");
+  }
+  getById(id: string): Promise<Task | null> {
+    throw new Error("Method not implemented.");
+  }
+  create(task: Task): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+  update(task: Task): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+  delete(task: Task): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
 }
