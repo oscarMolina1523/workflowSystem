ac40c904d7fccae7ebffee8baa2266dfce1a1e17
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-09-24 20:48:10 -0600

    feat: create database at project start

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,25 @@
 import express from 'express';
+import { initializeDatabase } from './Infrastructure.Endpoint/database/init_db';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+async function startServer() {
+    try {
+        //console.log("Iniciando la base de datos...");
+        await initializeDatabase(); // Ejecutar la inicialización aquí
+        //console.log("Base de datos inicializada.");
+
+        // Inicia el servidor solo después de que la base de datos esté lista
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
+        });
+    } catch (error) {
+        console.error("Error al iniciar el servidor:", error);
+        process.exit(1); // Salir de la aplicación si hay un error crítico
+    }
+}
+
+// Llama a la función de inicio
+startServer();
