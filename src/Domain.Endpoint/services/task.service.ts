f30d550cf0855e03c7f212eacf79307229e32a1c
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-09-25 17:42:26 -0600

    feat: inject interface of task repository in task service

--- src/Domain.Endpoint/services/task.service.ts
+++ src/Domain.Endpoint/services/task.service.ts
@@ -1,23 +1,34 @@
+import { inject, injectable } from "tsyringe";
 import { TaskDTO } from "../dtos/task.dto";
 import Task from "../entities/task.model";
+import { ITaskRepository } from "../interfaces/repositories/taskRepository.interface";
 import { ITaskService } from "../interfaces/services/taskService.interface";
 import { ServiceResult } from "../utils/serviceResult.type";
 
+@injectable()
 export default class TaskService implements ITaskService {
-    getChickens(): Promise<Task[]> {
-        throw new Error("Method not implemented.");
-    }
-    getById(id: string): Promise<Task | null> {
-        throw new Error("Method not implemented.");
-    }
-    addChicken(chicken: TaskDTO): Promise<ServiceResult<Task>> {
-        throw new Error("Method not implemented.");
-    }
-    updateChicken(id: string, chicken: TaskDTO): Promise<ServiceResult<Task | null>> {
-        throw new Error("Method not implemented.");
-    }
-    deleteChicken(id: string): Promise<{ success: boolean; message: string; }> {
-        throw new Error("Method not implemented.");
-    }
+  private readonly _taskRepository: ITaskRepository;
 
+  constructor(@inject('ITaskRepository') taskRepository: ITaskRepository) {
+    this._taskRepository = taskRepository;
+  }
+
+  getChickens(): Promise<Task[]> {
+    throw new Error("Method not implemented.");
+  }
+  getById(id: string): Promise<Task | null> {
+    throw new Error("Method not implemented.");
+  }
+  addChicken(chicken: TaskDTO): Promise<ServiceResult<Task>> {
+    throw new Error("Method not implemented.");
+  }
+  updateChicken(
+    id: string,
+    chicken: TaskDTO
+  ): Promise<ServiceResult<Task | null>> {
+    throw new Error("Method not implemented.");
+  }
+  deleteChicken(id: string): Promise<{ success: boolean; message: string }> {
+    throw new Error("Method not implemented.");
+  }
 }
