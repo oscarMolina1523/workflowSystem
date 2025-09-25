5ede7f081a53e43aa2b15337d88a6704b1549553
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-09-25 17:57:02 -0600

    feat: add swagger file to control routes

---
+++ src/WebApi/swagger/swagger.ts
@@ -0,0 +1,40 @@
+import swaggerJsdoc from "swagger-jsdoc";
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "SYSTEM API",
+      version: "1.0.0",
+      description: "API to manage tasks by roles, permissions, and work areas",
+      contact: {
+        name: "Oscar Molina",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+        description: "Local server",
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          // ✅ Nombre del esquema de seguridad
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [], // ✅ Aplica globalmente a todos los endpoints
+      },
+    ],
+  },
+  apis: ["dist/WebApi/routes/*.js","src/WebApi/routes/*.ts", ],
+};
+
+const specs = swaggerJsdoc(options);
+export default specs;
