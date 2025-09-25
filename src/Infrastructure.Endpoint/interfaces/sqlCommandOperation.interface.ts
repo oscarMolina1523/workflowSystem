f8ffb775b5e46f5b403104c701d9428a6487bf2d
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-09-25 16:44:34 -0600

    feat: add sql command operation interface to implement in builder operation

---
+++ src/Infrastructure.Endpoint/interfaces/sqlCommandOperation.interface.ts
@@ -0,0 +1,29 @@
+import BaseModel from "../../Domain.Endpoint/entities/base.model";
+import { SqlReadOperation, SqlWriteOperation } from "../builders/sqlOperations.enum";
+import { EntityType } from "../utils/entityTypes";
+import { SqlCommand } from "./sqlCommand.interface";
+
+export interface ISqlCommandOperationBuilder {
+  From<TEntity extends BaseModel>(entityType: EntityType, entity: TEntity): IHaveSqlWriteOperation;
+  Initialize(entityType: EntityType): IHaveSqlReadOperation;
+}
+
+export interface IHaveSqlWriteOperation {
+  WithOperation(operation: SqlWriteOperation): IExecuteWriteBuilder;
+}
+
+export interface IExecuteWriteBuilder {
+  BuildWritter(): SqlCommand; 
+}
+
+export interface IHaveSqlReadOperation {
+  WithOperation(operation: SqlReadOperation): IHavePrimaryKeyValue;
+}
+
+export interface IHavePrimaryKeyValue extends IExecuteReadBuilder {
+  WithId(id: string): IExecuteReadBuilder;
+}
+
+export interface IExecuteReadBuilder {
+  BuildReader(): SqlCommand; 
+}
