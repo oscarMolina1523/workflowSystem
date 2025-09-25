2a1d3c561524394f9a7cb8d488ed94d54250a230
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-09-25 17:32:15 -0600

    feat: add optional field

--- src/Domain.Endpoint/entities/task.model.ts
+++ src/Domain.Endpoint/entities/task.model.ts
@@ -3,13 +3,13 @@ import { Status } from "./status.enum";
 
 export default class Task extends BaseModel{
     title:string;
-    description:string
     status: Status;
     areaId:string;
     createdBy:string;
     assignedTo:string;
+    description?:string  | undefined;
 
-    constructor(id:string, title:string, description:string, status:Status, areaId:string, createdBy:string, assignedTo:string){
+    constructor(id:string, title:string, status:Status, areaId:string, createdBy:string, assignedTo:string, description?:string,){
         super(id);
         this.title=title;
         this.description=description;
