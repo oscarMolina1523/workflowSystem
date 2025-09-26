d0870fab8a2b4a1c875d3591b6b628d95c1a455d
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-09-25 19:48:36 -0600

    feat: add getTaskById method in controller

--- src/WebApi/controllers/task.controller.ts
+++ src/WebApi/controllers/task.controller.ts
@@ -11,11 +11,31 @@ export default class TaskController {
 
   getTasks = async (req: Request, res: Response) => {
     try {
-      const chickens = await this.service.getTasks();
-      res.status(200).json({ success: true, data: chickens });
+      const tasks = await this.service.getTasks();
+      res.status(200).json({ success: true, data: tasks });
     } catch (error) {
       console.log(error);
       res.status(500).json({ message: "Failed to get tasks" });
+    }
+  };
+
+  getTaskById = async (req: Request, res: Response) => {
+    const taskId: string | undefined = req.params.id;
+
+    if (!taskId) {
+      return res.status(400).json({ message: "Task ID is required." });
+    }
+
+    try {
+      const task = await this.service.getById(taskId);
+
+      if (task) {
+        res.status(200).json({ success: true, data: task });
+      } else {
+        res.status(404).json({ message: "task not found" });
+      }
+    } catch {
+      res.status(500).json({ message: "Failed to get task" });
     }
   };
 }
