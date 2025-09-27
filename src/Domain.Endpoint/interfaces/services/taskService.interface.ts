cdb16cb2bba5843f583de6b6e2a0ab4c4089f0c3
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-09-26 22:14:05 -0600

    feat: add method to get tasks by area in task service interface

--- src/Domain.Endpoint/interfaces/services/taskService.interface.ts
+++ src/Domain.Endpoint/interfaces/services/taskService.interface.ts
@@ -6,6 +6,7 @@ import { ServiceResult } from "../../utils/serviceResult.type";
 export interface ITaskService {
   getTasks(): Promise<Task[]>;
   getById(id: string): Promise<Task | null>;
+  getTaskByArea(areaId: string): Promise<Task[] | null>;
   addTask(task: TaskDTO): Promise<ServiceResult<Task>>;
   updateTask(id: string, task: TaskDTO): Promise<ServiceResult<Task | null>>;
   deleteTask(id: string): Promise<{ success: boolean; message: string }>;
