e1c03342dff03d5cf0f2381400c9b93a9e4017a2
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-09-26 22:13:39 -0600

    feat: add method to get tasks by areas in task repository interface

--- src/Domain.Endpoint/interfaces/repositories/taskRepository.interface.ts
+++ src/Domain.Endpoint/interfaces/repositories/taskRepository.interface.ts
@@ -3,6 +3,7 @@ import Task from "../../entities/task.model";
 export interface ITaskRepository {
   getAll(): Promise<Task[]>;
   getById(id: string): Promise<Task | null>;
+  getByAreaId(areaId: string): Promise<Task[]>;
   create(task: Task): Promise<void>;
   update(task: Task): Promise<void>;
   delete(task: Task): Promise<void>;
