6c123590ef3732a1ba68b3a8b915e859458940c6
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-09-27 12:11:45 -0600

    refactor: rename values in small letters

--- src/Domain.Endpoint/entities/status.enum.ts
+++ src/Domain.Endpoint/entities/status.enum.ts
@@ -1,7 +1,7 @@
 export enum Status {
   PENDING = "pending",
   IN_PROGRESS = "in_progress",
-  PENDING_VALIDATION = "PENDING_VALIDATION", 
-  DONE = "DONE",
-  REJECTED = "REJECTED",
+  PENDING_VALIDATION = "pending_validation", 
+  DONE = "done",
+  REJECTED = "rejected",
 }
