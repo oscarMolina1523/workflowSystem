c9c6db8117816c823bbb7213feda357c599cac81
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-09-27 12:15:48 -0600

    feat: add method to get task by status

--- src/Domain.Endpoint/interfaces/repositories/taskRepository.interface.ts
+++ src/Domain.Endpoint/interfaces/repositories/taskRepository.interface.ts
@@ -4,6 +4,7 @@ export interface ITaskRepository {
   getAll(): Promise<Task[]>;
   getById(id: string): Promise<Task | null>;
   getByAreaId(areaId: string): Promise<Task[]>;
+  getByStatus(status: string): Promise<Task[]>;
   create(task: Task): Promise<void>;
   update(task: Task): Promise<void>;
   delete(task: Task): Promise<void>;
