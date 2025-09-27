190eaaff5b3c63d6ee1a014f5048b8b2788c7273
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-09-27 11:50:35 -0600

    feat: add more status to validate

--- src/Domain.Endpoint/entities/status.enum.ts
+++ src/Domain.Endpoint/entities/status.enum.ts
@@ -1,5 +1,7 @@
 export enum Status {
-    PENDING = "pending",
-    IN_PROGRESS = "in_progress",
-    COMPLETED = "completed"
+  PENDING = "pending",
+  IN_PROGRESS = "in_progress",
+  PENDING_VALIDATION = "PENDING_VALIDATION", 
+  DONE = "DONE",
+  REJECTED = "REJECTED",
 }
