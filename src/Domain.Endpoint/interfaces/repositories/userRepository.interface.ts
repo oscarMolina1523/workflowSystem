63e825c96172afbf5030d2a05d2ec3b17a3c43b9
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-09-30 20:05:05 -0600

    feat: add method to get users by areaId in user entity

--- src/Domain.Endpoint/interfaces/repositories/userRepository.interface.ts
+++ src/Domain.Endpoint/interfaces/repositories/userRepository.interface.ts
@@ -4,6 +4,7 @@ export interface IUserRepository {
   getAll(): Promise<User[]>;
   getById(id: string): Promise<User | null>;
   getByEmail(email: string): Promise<User | null>;
+  getByAreaId(areaId: string): Promise<User[]>
   create(user: User): Promise<void>;
   update(user: User): Promise<void>;
   delete(user: User): Promise<void>;
